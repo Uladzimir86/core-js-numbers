326096ce2f2f396c636df453b3d777533dad77f1
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2024-11-20 23:42:09 +0300

    solve isPowerOfTwo

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -320,8 +320,9 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  const pow = Math.log2(num);
+  return Math.floor(pow) === pow;
 }
 
 /**
